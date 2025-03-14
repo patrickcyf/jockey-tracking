f931aed4010410332857755606302be1a8839e16
Patrick Cheung <patrickcyf@gmail.com>
2025-03-14 21:17:40 +0800

    track jockeys using ByteTrack

--- predict.py
+++ predict.py
@@ -1,6 +1,9 @@
 import argparse
 
+from tqdm import tqdm
 from ultralytics import YOLO
+
+import supervision as sv
 
 def process_video(
     source_weights_path: str,
@@ -11,6 +14,33 @@ def process_video(
 ) -> None:
     model = YOLO(source_weights_path)
 
+    tracker = sv.ByteTrack()
+    box_annotator = sv.BoxCornerAnnotator()
+    label_annotator = sv.LabelAnnotator()
+    frame_generator = sv.get_video_frames_generator(source_path=source_video_path)
+    video_info = sv.VideoInfo.from_video_path(video_path=source_video_path)
+
+    with sv.VideoSink(target_path=target_video_path, video_info=video_info) as sink:
+        for frame in tqdm(frame_generator, total=video_info.total_frames):
+            results = model(
+                frame, verbose=False, conf=confidence_threshold, iou=iou_threshold
+            )[0]
+            detections = sv.Detections.from_ultralytics(results)
+            detections = tracker.update_with_detections(detections)
+
+            labels = [
+                f"#{tracker_id} {confidence:0.2f}"
+                for tracker_id, confidence
+                in zip(detections.tracker_id, detections.confidence)
+            ]
+
+            annotated_frame = box_annotator.annotate(
+                scene=frame.copy(), detections=detections
+            )
+            frame = label_annotator.annotate(
+                scene=annotated_frame, detections=detections, labels=labels
+            )
+            sink.write_frame(frame)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
